d7727347946d49056b97c12e6f9b3a64241d775c
asifvora <asifvora0@gmail.com>
2019-12-08 01:48:05 +1400

    feat : Added more skills and experiences.

--- src/data/data.js
+++ src/data/data.js
@@ -6,7 +6,7 @@ const sectionTypes = ['Profile', 'Skills', 'Educations', 'Experience', 'Projects
 
 const profile = {
     myName: 'asifvora',
-    profession: 'JavaScript developer',
+    profession: 'Software Engineer',
     email: 'asifvora0@gmail.com',
     city: 'viramgam',
     postalCode: '382150'
@@ -19,6 +19,7 @@ const socialNetworks = {
     twitter: 'https://twitter.com/007_dark_shadow',
     instagram: 'https://www.instagram.com/007_dark_shadow/',
     facebook: 'https://www.facebook.com/asif.vora.92',
+    photography: 'https://www.instagram.com/loves_photography__/',
 };
 
 
@@ -30,12 +31,15 @@ const css = { level: 1.0, name: 'CSS' };
 const scss = { level: 1.0, name: 'SCSS' };
 const jquery = { level: 1.0, name: 'jQuery' };
 const bootstrap = { level: 1.0, name: 'Bootstrap' };
+const sc = { level: 0.7, name: 'Styled Component' };
 
 const js = { level: 1.0, name: 'JavaScript' };
 const es = { level: 1.0, name: 'ES6' };
 const react = { level: 1.0, name: 'ReactJS' };
 const redux = { level: 1.0, name: 'ReduxJS' };
 const rn = { level: 0.8, name: 'React Native' };
+const electron = { level: 0.8, name: 'Electron' };
+const mobX = { level: 1, name: 'MobX' };
 const vuejs = { level: 1.0, name: 'VueJS' };
 const vuex = { level: 1.0, name: 'Vuex' };
 const ng = { level: 1.0, name: 'Angular7' };
@@ -47,31 +51,37 @@ const node = { level: 1.0, name: 'NodeJS' };
 const express = { level: 1.0, name: 'ExpressJS' };
 const sails = { level: 1.0, name: 'SailsJS' };
 const websockets = { level: 0.8, name: 'WebSockets' };
+const pusher = { level: 0.8, name: 'JavaScript Pusher' };
 const ionic = { level: 0.8, name: 'Ionic' };
 
-const lambda = { level: 1.0, name: 'AWS Lambda' };
+const lambda = { level: 0.8, name: 'AWS Lambda' };
 const api = { level: 1.0, name: 'REST APIs' };
-const gql = { level: 1.0, name: 'GraphQL' };
+const gql = { level: 0.5, name: 'GraphQL' };
 const realmdb = { level: 1.0, name: 'RealmDB' };
-const mysql = { level: 1.0, name: 'MySQL' };
-const mongodb = { level: 1.0, name: 'MongoDB' };
-const psql = { level: 1.0, name: 'PostgreSQL' };
-const fb = { level: 0.5, name: 'Firebase' };
+const mysql = { level: 0.8, name: 'MySQL' };
+const mongodb = { level: 0.8, name: 'MongoDB' };
+const psql = { level: 0.7, name: 'PostgreSQL' };
+const fb = { level: 0.8, name: 'Firebase' };
+const fs = { level: 0.8, name: 'Full Story' };
+const intercom = { level: 0.8, name: 'Intercom' };
+const mp = { level: 0.8, name: 'Mix Panel' };
+const ga  = { level: 0.8, name: ' Google Analytics' };
 
 const php = { level: 0.7, name: 'PHP' };
 const cakephp = { level: 0.7, name: 'Cake PHP' };
 
-const git = { level: 0.8, name: 'Git' };
-const svn = { level: 0.8, name: 'SVN' };
-const sourcetree = { level: 0.8, name: 'Source Tree' };
+const git = { level: 1, name: 'Git' };
+const svn = { level: 1, name: 'SVN' };
+const bb = { level: 1, name: 'Bit Bucket' };
+const sourcetree = { level: 1, name: 'Source Tree' };
 const adbphotoshop = { level: 0.7, name: 'Adobe Photoshop' };
 const adbdesign = { level: 0.7, name: 'Adobe InDesign' };
 
 const skills = {
-    html, css, scss, bootstrap, jquery, js, es, react, redux,
-    rn, vuejs, vuex, ng, typescript, webpack, sw, mern, node, express, sails,
-    websockets, ionic, lambda, api, gql, realmdb, mysql, mongodb, psql, fb,
-    php, cakephp, git, svn, sourcetree, adbphotoshop, adbdesign
+    html, css, scss, bootstrap, sc, jquery, js, es, react, redux,
+    rn,electron, mobX, vuejs, vuex, ng, typescript, webpack, sw, mern, node, express, sails,
+    websockets, pusher, ionic, lambda, api, gql, realmdb, mysql, mongodb, psql, fb, fs, intercom, mp, ga,
+    php, cakephp, git, svn, bb, sourcetree, adbphotoshop, adbdesign
 };
 
 
@@ -127,12 +137,20 @@ const educations = [
 *******************/
 const experience = [
     {
+        company: 'Simform Solutions',
+        remote: false,
+        position: 'Software Engineer',
+        description: `I am working as a Software Engineer for building web applications.`,
+        startsAt: '2019-06-18',
+        endsAt: undefined,
+    },
+    {
         company: 'Multidots Solutions Pvt Ltd',
         remote: false,
         position: 'JavaScript Developer',
         description: `I am working as a JavaScript Developer for building web applications.`,
         startsAt: '2017-07-01',
-        endsAt: undefined,
+        endsAt: '2019-05-31',
     },
     {
         company: 'Multidots Solutions Pvt Ltd',
@@ -153,7 +171,7 @@ const projects = [
     {
         name: 'My Website',
         url: 'https://asifvora.netlify.com',
-        description: `My personal website`,
+        description: `My website`,
         startsAt: '2019',
         endsAt: undefined,
         skills: {
